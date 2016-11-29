8ac9d70a95c8c3e5631e27243ae653085b935793
dpalomar <dpalomar@gmail.com>
2016-11-29 17:58:05 +0100

    Reviewed lab

--- lab04/environments/production/manifests/site.pp
+++ lab04/environments/production/manifests/site.pp
@@ -1,23 +1,25 @@
 node /^puppet/{
 	include puppetserver
-  class { 'puppetdb': }
-  # Configure the Puppet master to use puppetdb
-   class { 'puppetdb::master::config':
-   		  puppetdb_server => 'puppet',
-		  puppetdb_port   => 8081,
-    }
+	class { 'puppetdb': 
+		listen_address => "0.0.0.0",
+	}
+ #  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config':
+	puppetdb_server => 'puppet',
+
+   }
     class { 'apache': }
-	class { 'apache::mod::wsgi': }
-	# Configure Puppetboard
-	# Access Puppetboard through pboard.example.com
-	class { 'puppetboard':
-	  # manage_git        => true,
-	  # manage_virtualenv => true,
-	}
-	class { 'puppetboard::apache::vhost':
-	 vhost_name => 'pboard.example.com',
-	 port       => 80,
-	}
+    class { 'apache::mod::wsgi': }
+    # Configure Puppetboard
+    class { 'puppetboard':
+      manage_git        => true,
+      manage_virtualenv => true,
+    }
+    # Access Puppetboard through pboard.example.com
+    class { 'puppetboard::apache::vhost':
+     vhost_name => 'pboard.example.com',
+     port       => 80,
+    }
 }
 node /^web/{
 	include webserver
